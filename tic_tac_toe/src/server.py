9372530889f4d558020a1b74aebd3eeb05867798
leankyr <leankyr@gmail.com>
2018-10-21 14:38:54 +0300

    start of work

--- tic_tac_toe/src/server.py
+++ tic_tac_toe/src/server.py
@@ -19,12 +19,10 @@ def decide_winner(win):
 
 
 def handle_request(req):
-
-    rospy.longinfo("Requesting move...i")
-    #for invalid move post table
+    
     if req.x > 3 or req.y > 3:
         mes = 'showing Table'
-        rospy.longinfo(mes)
+        rospy.loginfo(mes)
         return moveResponse(mes,table)
      # calculating change in table 
     pos = 3*(req.x-1) + req.y - 1
@@ -53,30 +51,30 @@ def handle_request(req):
 def evaluate_winner():
     # Evaluate Rows
     if table[0] == table[1] == table[2] != 0:
-        rospy.longinfo ('Winner Found at first row')
+        rospy.loginfo ('Winner Found at first row')
         return table[0]
     if table[3] == table[4] == table[5] != 0:
-        rospy.longinfo ('Winner Found at second row')
+        rospy.loginfo ('Winner Found at second row')
         return table[3]
     if table[6] == table[7] == table[8] != 0:
-        rospy.longinfo ('Winner Found at third row')
+        rospy.loginfo ('Winner Found at third row')
         return table[6]
     # Evaluate Cols
     if table[0] == table[3] == table[6] != 0:
-        rospy.longinfo ('Winner Found at first col')
+        rospy.loginfo ('Winner Found at first col')
         return table[0]
     if table[1] == table[4] == table[7] != 0:
-        rospy.longinfo ('Winner Found at second col')
+        rospy.loginfo ('Winner Found at second col')
         return table[1]
     if table[2] == table[5] == table[8] != 0:
-        rospy.longinfo ('Winner Found at third col')
+        rospy.loginfo ('Winner Found at third col')
         return table[2]
     # Evaluate Diagonals 
     if table[0] == table[4] == table[8] != 0:
-        rospy.longinfo ('Winner Found at diag 1')
+        rospy.loginfo ('Winner Found at diag 1')
         return table[0]
     if table[2] == table[4] == table[6] != 0:
-        rospy.longinfo ('Winner Found at diag 2')
+        rospy.loginfo ('Winner Found at diag 2')
         return table[2]
 
     return 0
